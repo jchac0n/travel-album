d199c1d4fccefdf1ec054627ba884512b3c31efc
jchac0n <88922632+jchac0n@users.noreply.github.com>
2023-02-06 20:43:51 -0700

    adding static test images

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,18 +1,19 @@
-const tempTxt = [
-  "album1Loc",
-  "album2Loc",
-  "album3Loc",
-  "album5Loc",
-  "album6Loc",
-  "album7Loc",
-];
+import img0 from "../test_images/pic0.jpg";
+import img1 from "../test_images/pic1.jpg";
+import img2 from "../test_images/pic2.jpg";
+import img3 from "../test_images/pic3.png";
+import img4 from "../test_images/pic4.jpg";
+import img5 from "../test_images/pic5.jpg";
+import img6 from "../test_images/pic6.jpg";
+
+const images = [img0, img1, img2, img3, img4, img5, img6];
 
 const Album = () => {
   return (
     <div className="grid-container">
-      {tempTxt.map((txt, idx) => (
+      {images.map((img, idx) => (
         <div className="grid-item" key={idx}>
-          {txt}
+          <img src={img} alt="" />
         </div>
       ))}
     </div>
