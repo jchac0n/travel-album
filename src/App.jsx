7270113a685a07d6bc8babb53786bfc554a13089
jchac0n <88922632+jchac0n@users.noreply.github.com>
2023-02-12 17:53:40 -0700

    adding reeeaally ugly google sign in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import ReactDOM from "react-dom";
 import Map from "./Map";
 import Album from "./Album";
-
+import GoogleInfo from "./GoogleInfo";
 import { useLoadScript } from "@react-google-maps/api";
 
 const App = () => {
   const { isLoaded } = useLoadScript({
-    //googleMapsApiKey: "",
+    //googleMapsApiKey: "", /TODO: This doesn't work
     googleMapsApiKey: import.meta.env.VITE_GOOGLEMAPS_API_KEY,
   });
 
   return (
     <div>
       {!isLoaded ? (
-        <div>Loading...</div>
+        <div>Map Loading...</div>
       ) : (
         <div>
           <Map />
         </div>
       )}
+      <div>
+        <GoogleInfo />
+      </div>
       <div>
         <Album />
       </div>
